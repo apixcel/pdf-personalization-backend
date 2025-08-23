76d05f863cd4c0a3ea449c0765a1155176b53190
Nazmul Islam Sakib <sakibsarkar>
2025-08-23 12:46:16 +0600

    feat: Pdf form update

--- src/copy.assets.ts
+++ src/copy.assets.ts
@@ -2,7 +2,7 @@ import fs from "fs-extra";
 
 async function copyTemplates() {
   try {
-    await fs.copy("src/app/templates", "dist/templates");
+    await fs.copy("src/app/templates", "dist/app/templates");
     console.log("Templates copied successfully!");
   } catch (err) {
     console.error("Error copying templates:", err);
